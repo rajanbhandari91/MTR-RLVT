e7cfc92fb3f44549a04cead460766b2c0c0400fc
Geronim0-0 <hcm0032@auburn.edu>
2024-10-21 22:59:28 -0500

    AFDD Hydraulics weight function
    changed hydraulics weight function to use AFDD model. have not integrated with main yet.

--- Aircraft Weight Functions/EvalHydWeight_lb.m
+++ Aircraft Weight Functions/EvalHydWeight_lb.m
@@ -1,4 +1,4 @@
-function W_hyd_lb = EvalHydWeight_lb(W_dg,M)
+function W_hyd = EvalHydWeight_lb(X)
     % Reviewed 11/19/2021 2129 hrs
     % 1. Raymer, General Aviation Weights, Pg. 576
 
@@ -7,15 +7,53 @@ function W_hyd_lb = EvalHydWeight_lb(W_dg,M)
     % M = Design Mach Number
 
 
-if M < 0.4
-    K_h = 0.05;                                                            
-elseif M >= 0.4 && M < 0.8
-     K_h = 0.11;
-elseif M >= 0.8
-    K_h = 0.12;
-else
-    K_h = 0.013;
+% if M < 0.4
+%     K_h = 0.05;                                                            
+% elseif M >= 0.4 && M < 0.8
+%      K_h = 0.11;
+% elseif M >= 0.8
+%     K_h = 0.12;
+% else
+%     K_h = 0.013;
+% end
+% 
+%     W_hyd_lb = K_h*(W_dg^0.8)*(M^0.5);
+
+
+
+% Author: Cole McCormick
+% Last Update: 10/21/2024
+
+%% Parameters %%
+
+% W_EQhyd = hydraulic equipement weight (fixed) {lb}
+% W_mb = fight controls boost mechanism weight {lb}
+% w_fc = flight controls weight {lb}
+% f_hyd = hydraulics weight (fraction boost mechanism weight) (typical = 0.4, tiltwing)
+% X_hyd = hydraulics weight factor (1 if present, else 0)
+
+%% Unpack Inputs %%
+
+W_EQhyd = X(1);
+W_mb = X(2);
+w_fc = X(3);
+f_hyd = X(4);
+X_hyd = X(5);
+
+%% AFDD Weight Model Hydraulic Group %%
+% NDARC NASA Design and Analysis of Rotorcraft, Wayne Johnson,
+% pg.239
+% Consists of hydrualics for flight controls and equipement.
+
+% AFDD82 Parametric Model
+
+switch ConfigFlag
+    case 0 % Fixed Wing or Tilt Rotor
+        W_hyd = X_hyd * f_hyd * W_mb;
+    case 1 % Tilt Wing
+        W_hyd = X_hyd * f_hyd * w_fc;
 end
 
-    W_hyd_lb = K_h*(W_dg^0.8)*(M^0.5);
+W_hyd = W_hyd + W_EQhyd;
+
 end
